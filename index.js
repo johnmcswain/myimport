19f9550ae35b1cf4cfa6024065e8cda8f48a92c4
John McSwain <john.i.mcswain@gmail.com>
2018-10-04 11:32:50 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+var express = require("express");
+var bodyParser = require("body-parser");14
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 8889,
+  user: "root",
+  password: "root",
+  database: "movie_planner_db"
+});
+
+
+connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+  
+    console.log("connected as id " + connection.threadId);
+  });
+
+module.exports = {
+    express: express,
+    bodyParser: bodyParser,
+    app: app,
+    connection: connection,
+    PORT: PORT
+}
